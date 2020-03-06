41255dc18e5dbb17c6d2410d87f5adc9d873e1f7
grumman thegrumman <asl.grumman@outlook.de>
2020-03-06 20:31:07 +0100

    Added Smelting Recipe for custom block: Doesn't seem to work.

--- src/main/java/de/firstmod/testmod/init/FirstSmelting.java
+++ src/main/java/de/firstmod/testmod/init/FirstSmelting.java
@@ -12,5 +12,8 @@ public class FirstSmelting
         GameRegistry.addSmelting(Blocks.BOOKSHELF, new ItemStack(ItemInit.firstItem, 4), 2F);
         GameRegistry.addSmelting(Blocks.BEDROCK, new ItemStack(Items.APPLE, 4), 3F);
         GameRegistry.addSmelting(ItemInit.firstItem, new ItemStack(Items.APPLE, 4), 4F);
+
+        //This doesn't seem to work
+        GameRegistry.addSmelting(FirstBlocks.firstblock, new ItemStack(Items.APPLE, 4), 4F);
     }
 }
